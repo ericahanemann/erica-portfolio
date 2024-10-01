94dc3dd768ad2aa568b8af0f769b6cabdeac95eb
Érica <ericavhanemann@gmail.com>
2024-10-01 15:16:39 -0300

    feat: implement formspree on contact form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,7 +1,8 @@
 import styles from "./Contact.module.css";
 import { TranslationContent } from "../../types/translations";
-import { ChangeEvent, FormEvent, useEffect } from "react";
+import { ChangeEvent, useEffect } from "react";
 import { useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 interface ContactProps {
   content: TranslationContent;
@@ -11,17 +12,10 @@ export default function Contact({ content }: ContactProps) {
   const [contact, setContact] = useState({
     name: "",
     email: "",
-    subject: "StaticForms - Contact Form",
-    honeypot: "", // if any value received in this field, form submission will be ignored.
     message: "",
-    replyTo: "@", // this will set replyTo of email to email address entered in the form
-    accessKey: "you-access-key", // get your access key from https://www.staticforms.xyz
   });
 
-  const [response, setResponse] = useState({
-    type: "",
-    message: "",
-  });
+  const [state, handleSubmit] = useForm("mzzpnokz");
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -31,92 +25,78 @@ export default function Contact({ content }: ContactProps) {
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => setContact({ ...contact, [e.target.name]: e.target.value });
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const res = await fetch("https://api.staticforms.xyz/submit", {
-        method: "POST",
-        body: JSON.stringify(contact),
-        headers: { "Content-Type": "application/json" },
-      });
-
-      const json = await res.json();
-
-      if (json.success) {
-        setResponse({
-          type: "success",
-          message: "Thank you for reaching out to us.",
-        });
-      } else {
-        setResponse({
-          type: "error",
-          message: json.message,
-        });
-      }
-    } catch (e) {
-      console.log("An error occurred", e);
-      setResponse({
-        type: "error",
-        message: "An error occured while submitting the form",
-      });
-    }
-  };
-
   return (
     <main className={styles.main}>
       <section className={styles.contact}>
         <h3 className={styles.contactTitle}>{content.contact.title}</h3>
         <div className={styles.contactContainer}>
-          <div className={styles.contactInfo}>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
-              dolorum, molestiae facere impedit sed similique, harum nulla
-              asperiores id fugiat rerum esse ipsa labore fugit iusto voluptatum
-              in quas est. Lorem ipsum dolor sit, amet consectetur adipisicing
-              elit. Nostrum, exercitationem voluptate? Vero, magnam recusandae
-              soluta, eveniet itaque sit quos, dolor amet explicabo sint quas
-              animi eos repellat exercitationem! Ipsum, nisi?
-            </p>
-          </div>
-
           <div className={styles.formContainer}>
-            <form
-              action="https://api.staticforms.xyz/submit"
-              method="post"
-              onSubmit={handleSubmit}
-            >
-              <div className={styles.field} style={{ display: "none" }}>
-                <label className={styles.label}>Title</label>
-                <input
-                  className={styles.input}
-                  type="text"
-                  name="honeypot"
-                  style={{ display: "none" }}
-                  onChange={handleChange}
-                />
-                <input type="hidden" name="subject" onChange={handleChange} />
-              </div>
-              
+            {state.succeeded ? (
+              <p className={styles.statusMessage}>
+                já recebi seu contato! retornarei assim que possível ;)
+              </p>
+            ) : (
+              ""
+            )}
+            {state.errors ? (
+              <p className={styles.statusMessage}>
+                algo deu errado! se necessário, entre em contato diretamente no
+                meu email
+              </p>
+            ) : (
+              ""
+            )}
+            <div className={styles.formInfo}>
+              <p>
+                Caso tenha se interessado pelo meu trabalho e queira conversar
+                sobre uma ideia de projeto ou parceria, preencha o formulário ou
+                então entre em contato diretamente pelo email{" "}
+                <strong>ericavhanemann@gmail.com</strong>
+              </p>
+            </div>
+            <form method="POST" onSubmit={handleSubmit}>
               <div className={styles.field}>
-                <label className={styles.label}>Your Name</label>
+                <label className={styles.label} htmlFor="name">
+                  Your Name
+                </label>
                 <input
                   className={styles.input}
                   type="text"
                   name="name"
+                  value={state.succeeded || state.errors ? " " : contact.name}
                   placeholder=" "
                   onChange={handleChange}
                   required
+                  disabled={state.succeeded || state.errors ? true : false}
+                />
+                <ValidationError
+                  prefix="Name"
+                  field="name"
+                  errors={state.errors}
                 />
               </div>
               <div className={styles.field}>
-                <label className={styles.label}>Your Email</label>
+                <label className={styles.label} htmlFor="email">
+                  Your Email
+                </label>
                 <input
                   className={styles.input}
                   type="email"
                   name="email"
+                  value={state.succeeded || state.errors ? " " : contact.email}
                   placeholder=" "
                   onChange={handleChange}
                   required
+                  disabled={
+                    state.succeeded || state.errors || state.submitting
+                      ? true
+                      : false
+                  }
+                />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
                 />
               </div>
               <div className={styles.field}>
@@ -126,15 +106,36 @@ export default function Contact({ content }: ContactProps) {
                 <textarea
                   className={styles.textarea}
                   name="message"
+                  value={
+                    state.succeeded || state.errors ? " " : contact.message
+                  }
                   placeholder=" "
                   onChange={handleChange}
-                  rows={6}
+                  rows={4}
                   cols={100}
                   required
+                  disabled={
+                    state.succeeded || state.errors || state.submitting
+                      ? true
+                      : false
+                  }
+                />
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
                 />
               </div>
               <div className={styles.control}>
-                <button className={styles.submitButton} type="submit">
+                <button
+                  className={styles.submitButton}
+                  type="submit"
+                  disabled={
+                    state.succeeded || state.errors || state.submitting
+                      ? true
+                      : false
+                  }
+                >
                   Submit
                 </button>
               </div>
